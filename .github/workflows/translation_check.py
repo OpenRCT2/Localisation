698c5f2387a3271f33c8923156ad04b461a1a16c
Marcin Kunert <the.martines@gmail.com>
2020-10-17 10:47:49 +0200

    Script refactoring

--- .github/workflows/translation_check.py
+++ .github/workflows/translation_check.py
@@ -8,6 +8,10 @@ import filecmp
 import os
 import re
 
+MASTER_LANG_DIR = "master/data/language"
+PR_LANG_DIR = "pr/data/language"
+SPECIAL_KEYS = ['STR_SCNR', 'STR_PARK', 'STR_DTLS', 'STR_NAME']
+
 languages = []
 
 
@@ -15,8 +19,8 @@ def read_languages_from_master_and_pr():
     """
     Reads languages from master and pr branches
     """
-    read_languages_from_filenames("master/data/language")
-    read_languages_from_filenames("pr/data/language")
+    read_languages_from_filenames(MASTER_LANG_DIR)
+    read_languages_from_filenames(PR_LANG_DIR)
 
 
 def read_languages_from_filenames(dir_to_search):
@@ -42,8 +46,6 @@ def file_to_dict(filename):
     lines = [line.strip() for line in content]
     translations = {}
 
-    special_keys = ['STR_SCNR', 'STR_PARK', 'STR_DTLS', 'STR_NAME']
-
     previous_group_name = ''
 
     for line in lines:
@@ -57,7 +59,7 @@ def file_to_dict(filename):
         if line.find(':') != -1:
             split = line.split(':')
             key = split[0].strip()
-            if key in special_keys:
+            if key in SPECIAL_KEYS:
                 key = previous_group_name + key
             value = ':'.join(split[1:]).strip()
             translations[key] = value
@@ -120,10 +122,12 @@ def prepare_spoiler(summary, details):
 def run():
     """runs the script and outputs the result in result.md file"""
     read_languages_from_master_and_pr()
+    prepare_translation_report()
 
-    master_branch = count_translations('master/data/language', False)
-    pull_request = count_translations('pr/data/language', True)
 
+def prepare_translation_report():
+    master_branch = count_translations(MASTER_LANG_DIR, False)
+    pull_request = count_translations(PR_LANG_DIR, True)
     missing = prepare_spoiler('Missing', 'The translation is not added to translation file. '
                                          '(e.g. STR_9999 is in `en-GB` but is not available in given language)')
     not_needed = prepare_spoiler('Not needed', 'The translation file contains entries that are not in `en-GB` '
@@ -132,29 +136,23 @@ def run():
     same = prepare_spoiler('Same', 'The translation and source string is exactly the same.  '
                                    '(e.g. STR_9999 is `Umbrella` in both `en-GB` and given language). '
                                    'This may be desired in some cases (e.g. `April` is the same in English and German)')
-
     table_header = "| |" + missing + " | " + not_needed + " |" + same + "|\n"
     table_header += "|---|---|---|---|\n"
-
     languages_changed = []
-
     for lang in languages:
         filename = lang + '.txt'
-        file_master = 'master/data/language/' + filename
-        file_pr = 'pr/data/language/' + filename
+        file_master = MASTER_LANG_DIR + '/' + filename
+        file_pr = MASTER_LANG_DIR + '/' + filename
         if not os.path.exists(file_master) or not os.path.exists(file_pr):
             languages_changed.append(lang)
             continue
         if not filecmp.cmp(file_master, file_pr):
             languages_changed.append(lang)
-
     result = '#### Check results\n\n'
     result += "For details go to `Translation Check` -> `Details`. " \
               "Expand `Run checks` build stage and use the build-in search to find your language (e.g. `pl-PL`)\n\n"
     result += table_header
-
     other_table = "\n\n" + table_header
-
     for lang in languages:
         missing = format_result(master_branch['missing'][lang], pull_request['missing'][lang])
         not_needed = format_result(master_branch['not_needed'][lang], pull_request['not_needed'][lang])
@@ -166,12 +164,11 @@ def run():
             result += row
         else:
             other_table += row
-
     result += prepare_spoiler('Other translations', other_table)
-
     text_file = open("result.md", "w")
     text_file.write(result)
     text_file.close()
 
 
-run()
+if __name__ == "__main__":
+    run()
