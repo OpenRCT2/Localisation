d94f57b2aae2d0d04a191aa1ce2e8edcb276bf41
Tulio Leao <tupaschoal@gmail.com>
2023-04-11 14:40:30 -0300

    Remove outdated powershell script

--- langcheck/IErrorLogger.cs
+++
@@ -1,8 +0,0 @@
-namespace langcheck
-{
-    internal interface IErrorLogger
-    {
-        void LogWarning(int lineNumber, string message);
-        void LogError(int lineNumber, string message);
-    }
-}
