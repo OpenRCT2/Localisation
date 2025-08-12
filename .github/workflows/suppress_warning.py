af35a5727ea263eaaa5f0d8c66376e3ace90c358
Ríša Szlachta <risa.szlachta@gmail.com>
2025-08-12 12:37:18 +0200

    purge suppression list
    to have a clean start

--- .github/workflows/suppress_warning.py
+++ .github/workflows/suppress_warning.py
@@ -17,9 +17,6 @@ will break GitHub Action job
 
 Example of population:
 SUPPRESS_WARNING['qq-JJ'] = ['STR_0123', 'STR_5678', 'STR_8900']
-
-2996 - 3009 are COLOR-ABC strings used in Banner window and were pre-added
-for japanese and ukrainian which use different scripts (hiragana and cyrillic)
 """
 
 SUPPRESS_WARNING['ar-EG'] = []
@@ -36,10 +33,7 @@ SUPPRESS_WARNING['fr-FR'] = []
 SUPPRESS_WARNING['gl-ES'] = []
 SUPPRESS_WARNING['hu-HU'] = []
 SUPPRESS_WARNING['it-IT'] = []
-SUPPRESS_WARNING['ja-JP'] = ['STR_2996', 'STR_2997', 'STR_2998', 'STR_2999',
-                             'STR_3000', 'STR_3001', 'STR_3002', 'STR_3003',
-                             'STR_3004', 'STR_3005', 'STR_3006', 'STR_3007',
-                             'STR_3008', 'STR_3009']
+SUPPRESS_WARNING['ja-JP'] = []
 SUPPRESS_WARNING['ko-KR'] = []
 SUPPRESS_WARNING['nb-NO'] = []
 SUPPRESS_WARNING['nl-NL'] = []
@@ -48,10 +42,7 @@ SUPPRESS_WARNING['pt-BR'] = []
 SUPPRESS_WARNING['ru-RU'] = []
 SUPPRESS_WARNING['sv-SE'] = []
 SUPPRESS_WARNING['tr-TR'] = []
-SUPPRESS_WARNING['uk-UA'] = ['STR_2996', 'STR_2997', 'STR_2998', 'STR_2999',
-                             'STR_3000', 'STR_3001', 'STR_3002', 'STR_3003',
-                             'STR_3004', 'STR_3005', 'STR_3006', 'STR_3007',
-                             'STR_3008', 'STR_3009']
+SUPPRESS_WARNING['uk-UA'] = []
 SUPPRESS_WARNING['vi-VN'] = []
 SUPPRESS_WARNING['zh-CN'] = []
 SUPPRESS_WARNING['zh-TW'] = []
